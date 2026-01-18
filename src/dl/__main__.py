89c86999616a1e0b3cd1a6ef753a51189b876481
efrenlopes <efrenlopes@gmail.com>
2026-01-18 17:12:59 -0300

    O programa principal agora recebe um argumento do sistema e o imprime no terminal

--- src/dl/__main__.py
+++ src/dl/__main__.py
@@ -1,2 +1,10 @@
-if __name__ == "__main__":
-    print('Hello compiler!')
+import sys
+
+if __name__ == '__main__':
+    #Entrada
+    if len(sys.argv) != 2:
+        print('Argumentos inválidos! Esperado um caminho ' +
+              'de arquivo para um programa na linguagem DL.')
+        exit()
+    file_input = sys.argv[1]
+    print(file_input)
