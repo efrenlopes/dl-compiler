b8e8cf065a25e0c84b7174eb2a5034ba18d31028
efrenlopes <efrenlopes@gmail.com>
2026-01-21 09:14:06 -0300

    Adicionada a classe abstrata Visitor

---
+++ src/dl/tree/visitor.py
@@ -0,0 +1,33 @@
+from abc import ABC, abstractmethod
+
+class Visitor(ABC):
+    
+    @abstractmethod
+    def visit_program_node(self, node): pass
+    
+    @abstractmethod
+    def visit_block_node(self, node): pass
+    
+    @abstractmethod
+    def visit_decl_node(self, node): pass
+    
+    @abstractmethod
+    def visit_assign_node(self, node): pass
+    
+    @abstractmethod
+    def visit_if_node(self, node): pass
+    
+    @abstractmethod
+    def visit_write_node(self, node): pass
+    
+    @abstractmethod
+    def visit_var_node(self, node): pass
+    
+    @abstractmethod
+    def visit_literal_node(self, node): pass
+    
+    @abstractmethod
+    def visit_binary_node(self, node): pass
+    
+    @abstractmethod
+    def visit_convert_node(self, node): pass
