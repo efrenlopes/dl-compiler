18888a5a5ee7313c09f9e911307c04eced7f2038
efrenlopes <efrenlopes@gmail.com>
2026-03-13 10:17:34 -0300

    Revisão do pacote inter em andamento.

--- src/dlc/inter/basic_block.py
+++ src/dlc/inter/basic_block.py
@@ -1,3 +1,7 @@
+from __future__ import annotations
+
+from collections.abc import Generator
+
 from dlc.inter.instr import Instr
 
 
@@ -10,35 +14,34 @@ class BasicBlock:
         self.number = BasicBlock.__count
         #bb sections
         self.label_instr: Instr|None = None
-
-        self.phi_instrs = []
-        self.body_instrs = []
-        self.goto_instr = None
+        self.phi_instrs: list[Instr] = []
+        self.body_instrs: list[Instr] = []
+        self.goto_instr: Instr|None = None
         #links
-        self.successors = []
-        self.predecessors = []
+        self.successors: list[BasicBlock] = []
+        self.predecessors: list[BasicBlock] = []
 
-    def add_successor(self, bb):
+    def add_successor(self, bb: BasicBlock) -> None:
         if bb not in self.successors:
             self.successors.append(bb)
         if self not in bb.predecessors:
             bb.predecessors.append(self)
 
-    def __iter__(self):
-        yield self.label_instr
+    def __iter__(self) -> Generator[Instr, None, None]:
+        if self.label_instr:
+            yield self.label_instr
         for i in self.phi_instrs:
             yield i
         for i in self.body_instrs:
             yield i
-        if self.goto_instr is not None:
+        if self.goto_instr:
             yield self.goto_instr
     
-    # def __getitem__(self, index):
-    #     return self.instructions[index]
-    
+   
     def __str__(self) -> str:
         return f'bb{self.number}'
     
+
     def __repr__(self) -> str:
         return f'<{str(self)}>'
         #return f'<bb{self.number}: [{self.instructions[0]}]->[{self.instructions[-1]}]>'
